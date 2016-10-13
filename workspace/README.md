18dabe3d128808b5b497a4d16b6b9c4f9ec63b43
nalvescabrera <n.alvescabrera@gmail.com>
2016-10-13 19:56:59 -0300

    reupload

---
+++ workspace/README.md
@@ -0,0 +1,23 @@
+# natalia
+Projeto do Semestre
+Projeto do semestre Tema: Academia/Blog de yoga,saude,pilates
+
+Público-alvo:Adultos de todas as idades
+
+Objetivos do site:Divulgação de conteudo,divulgação de academia
+
+Funcionalidades do site: Cadastro de usuario, para que o mesmo possa comentar sobre divulgações e duvidas.
+
+Usuário Administrador:
+Administra pagina, controlando posts que serão publicados e comnetarios
+
+Usuário Logado:
+Permite comentarios em pagina
+
+Usuário Visitante:
+Visualização em posts, porem o mesmo nao permite comentar.
+Mapa de Navegação: 
+http://prnt.sc/cihl7x
+
+
+Wireframe:
---
+++ workspace/workspace/horario.php
@@ -0,0 +1,79 @@
+<?php include 'layouts/header.php'; ?>
+<?php include 'layouts/menu.php'; ?>
+<div class="container horario">
+<table class="table table-striped">
+<thead><p class="text-center">Horario</p></thead>
+<tr>
+ <td rowspan="3">Pilates</td>
+ <td>Segunda-feira</td>
+ <td>Terça-feira</td>
+ <td>Quarta-feira</td>
+ <td>Quinta-feira</td>
+ <td>Sexta-feira</td>
+ <td>Sábado</td>
+</tr>
+<tr>
+  <td>09:00 - 10:00</td>
+  <td>20:00 - 21:00</td>
+  <td>10:00 - 12:00</td>
+  <td>20:00 - 21:00</td>
+  <td>10:00 - 11:00</td>
+  <td>10:00 - 11:00</td>
+ </tr>
+ <tr>
+  <td>14:00 - 15:00</td>
+  <td> - </td>
+  <td>20:00 - 21:00</td>
+  <td> - </td>
+  <td>20:00 - 21:00</td>
+  <td> - </td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+ </tr>
+ <tr>
+  <td rowspan="3">Yoga</td>
+  <td>Segunda-feira</td>
+  <td>Terça-feira</td>
+  <td>Quarta-feira</td>
+  <td>Quinta-feira</td>
+  <td>Sexta-feira</td>
+  <td>Sábado</td>
+ </tr>
+ <tr>
+  <td>09:00 - 10:00</td>
+  <td>20:00 - 21:00</td>
+  <td>10:00 - 12:00</td>
+  <td>20:00 -21:00</td>
+  <td>10:00 - 11:00</td>
+  <td>10:00 - 11:00</td>
+ </tr>
+ <tr>
+  <td>14:00 - 15:00</td>
+  <td> - </td>
+  <td>20:00 - 21:00</td>
+  <td> - </td>
+  <td>20:00 - 21:00</td>
+  <td> - </td>
+ </tr>
+ <tr>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td></td>
+ </tr>
+</table>
+</div>
+
+<?php include 'layouts/footer.php'; ?>
+
